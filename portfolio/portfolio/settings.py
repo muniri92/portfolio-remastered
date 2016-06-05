252f457be4577196c9bd2c98f03490927ecf08f6
muniri92 <mibrah04@gmail.com>
2016-06-05 15:25:36 -0700

    connected the database

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -75,10 +75,26 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# DATABASES = {
+# 'default':dj_database_url.config(
+#     default=os.environ.get('PORTFOLIO_DATABASE_URL'))
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio',
+        'USER': os.environ.get('USER', 'MunirIbrahim'),
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
